7be120ef5cc18f6d0006775103559191e9f2ff00
biancaazuma <biancaazuma@gmail.com>
2024-07-22 11:18:25 -0300

    new version 2.7.1

--- Utils/AppConfig.cs
+++ Utils/AppConfig.cs
@@ -10,8 +10,7 @@ namespace _4RTools.Utils
         public static string Website = "https://ragnatales.com.br/";
         public static string GithubLink = "https://github.com/biancaazuma/4RTools-Ragnatales/tree/main";
         public static string DiscordLink = "https://discord.com/invite/bkG2RPGjEu";
-        public static string _4RClientsURL = "https://storage.googleapis.com/4rtools/supported_servers.json";
         public static string _4RLatestVersionURL = "https://api.github.com/repos/biancaazuma/4RTools-RagTools/releases/latest";
-        public static string Version = "v2.7.0";
+        public static string Version = "v2.7.1";
     }
 }
